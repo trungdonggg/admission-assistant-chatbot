8593d32a1570fa684dd47921fec30264aa0e9388
trungdonggg <trungdonggg@gmail.com>
2024-10-30 10:15:37 +0700

    llm

--- llm/app.py
+++ llm/app.py
@@ -1,34 +1,12 @@
-from quart import Quart, request, Response
-from llm.chat_template import ChatTemplate
-from llm.generator import Generator
+from flask import Flask
+from flask_restful import Api
 import config
+from llm.generator import GenerateResponse
 
-app = Quart(__name__)
-bot = Generator()
+app = Flask(__name__)
+api = Api(app)
 
-@app.route('/generate', methods=['POST'])
-async def generate_response():
-    data = await request.get_json()
-    query = data.get('query')
-    history = data.get('history')
-    context = data.get('context')
-
-    if not query:
-        return {"error": "No query provided"}, 400
-
-    prompt_components = ChatTemplate(
-        history=history,
-        context=context,
-        input=query
-    )
-
-    try:
-        response = await bot.ainvoke(prompt_components)
-
-        return {"response": response}, 200
-
-    except Exception as e:
-        return {"error": str(e)}, 500
+api.add_resource(GenerateResponse, '/generate')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=config.llm_api_port)
--- llm/generator.py
+++ llm/generator.py
@@ -1,4 +1,6 @@
 from typing import AsyncGenerator
+from flask import request
+from flask_restful import Resource
 from llm.utils import *
 from llm.chat_template import ChatTemplate
 
@@ -13,3 +15,31 @@ class Generator:
 
     async def ainvoke(self, prompt_components: ChatTemplate) -> str:
         return await self.generator.ainvoke(prompt_components.model_dump())
+
+
+class GenerateResponse(Resource):
+
+    def __init__(self):
+        self.bot = Generator()
+    async def post(self):
+        data = await request.get_json()
+        query = data.get('query')
+        history = data.get('history')
+        context = data.get('context')
+
+        if not query:
+            return {"error": "No query provided"}, 400
+
+        prompt_components = ChatTemplate(
+            history=history,
+            context=context,
+            input=query
+        )
+
+        try:
+            response = await self.bot.ainvoke(prompt_components)
+
+            return {"response": response}, 200
+
+        except Exception as e:
+            return {"error": str(e)}, 500
