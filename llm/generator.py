f8fefb9fa338c6d48c70f18f1f696374ea4b7e74
trungdonggg <trungdonggg@gmail.com>
2024-11-01 14:56:56 +0700

    done llm ?...

--- llm/generator.py
+++ llm/generator.py
@@ -1,19 +1,11 @@
-from typing import AsyncGenerator
 from flask import request
 from flask_restful import Resource
 from llm.utils import *
 from llm.chat_template import ChatTemplate
 
-
 class Generator:
     def __init__(self):
-        # self.generator = get_chatbot_prompt() | get_model() | streaming_parser()
         self.generator = get_chatbot_prompt() | get_model()
-
-
-    # async def astream(self, prompt_components: ChatTemplate) -> AsyncGenerator[str, None]:
-    #     async for word in self.generator.astream(prompt_components.model_dump()):
-    #         yield word
 
     async def ainvoke(self, prompt_components: ChatTemplate):
         return await self.generator.ainvoke(prompt_components.model_dump())
@@ -23,15 +15,15 @@ class Generator:
 
 
 class GenerateResponse(Resource):
-
     def __init__(self):
         self.bot = Generator()
-    async def post(self):
+
+    def post(self):
         data = request.json
 
         query = data.get('query')
-        history = data.get('history')
-        context = data.get('context')
+        history = data.get('history') or []
+        context = data.get('context') or ""
 
         if not query:
             return {"error": "No query provided"}, 400
@@ -43,7 +35,7 @@ class GenerateResponse(Resource):
         )
 
         try:
-            response = await self.bot.ainvoke(prompt_components)
+            response = self.bot.invoke(prompt_components)
 
             return {"response": response.content}, 200
 
