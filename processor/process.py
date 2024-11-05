0257dba1c40affecdf446cfbd2f02228d0cd5968
trungdonggg <trungdonggg@gmail.com>
2024-11-05 08:32:23 +0700

    update

--- processor/process.py
+++ processor/process.py
@@ -14,6 +14,7 @@ class Processor:
         
         # Split the document content into chunks
         text_chunks = await split_document(content=document_content)
+        print("text chunked: ")
         print(text_chunks)
 
         # Add document name and tag name to the database
@@ -23,23 +24,27 @@ class Processor:
                 tag_name=tag_name
             )
         )
+        print("document added to db")
 
-        # # Vectorize the text chunks
-        # vectors = await vectorize(
-        #     VectorizeRequest(
-        #         content=text_chunks
-        #     )
-        # )
+        # Vectorize the text chunks
+        vectors = await vectorize(
+            VectorizeRequest(
+                content=text_chunks
+            )
+        )
+        print("vectors: ")
+        print(len(vectors))
         
-        # # Add the document with chunks and vectors to the vector database
-        # await add_document_to_vectordb(
-        #     CreateDocumentRequestVectorDatabase(
-        #         document_name=document_name,
-        #         tag_name=tag_name,
-        #         chunks=text_chunks,
-        #         vectors=vectors
-        #     )
-        # )
+        # Add the document with chunks and vectors to the vector database
+        await add_document_to_vectordb(
+            CreateDocumentRequestVectorDatabase(
+                document_name=document_name,
+                tag_name=tag_name,
+                chunks=text_chunks,
+                vectors=vectors
+            )
+        )
+        print("document added to vectordb")
 
     async def delete_document(self, document_name: str):
         # Remove the document from the database and vector database
--- vector_database/run.sh
+++ vector_database/run.sh
@@ -1,4 +1,4 @@
-export PYTHONPATH="$HOME/ttu-chatbot"
+export PYTHONPATH=$(dirname "$(pwd -P)")
 
 pip install -r requirements.txt
 
